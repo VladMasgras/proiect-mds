a1ca1b0a1b956126c6a0ec87de6dce0b6f0add7a
Vlad Masgras <masgrasvlad@gmail.com>
2020-06-12 11:46:42 +0300

    Optiune logout pentru admin

--- app/src/main/java/com/example/proiect/MeniuAdmin.java
+++ app/src/main/java/com/example/proiect/MeniuAdmin.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MeniuAdmin extends AppCompatActivity {
 
     @Override
@@ -16,6 +18,7 @@ public class MeniuAdmin extends AppCompatActivity {
 
         Button btnAddTren = findViewById(R.id.admin_adauga_tren);
         Button btnAddVagon = findViewById(R.id.admin_adauga_vagon);
+        Button btnLogout = findViewById(R.id.btnlogout);
 
         btnAddTren.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,15 @@ public class MeniuAdmin extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MeniuAdmin.this, AdaugaVagoane.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MeniuAdmin.this, Login.class);
                 startActivity(intent);
             }
         });
